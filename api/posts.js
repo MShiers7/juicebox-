df42949883f19d1bc0a6e74de655e186b2886200
MShiers7 <martyshiers7@gmail.com>
2022-11-20 21:41:52 -0600

    Added utils file

--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,37 @@
 const express = require("express");
 const postsRouter = express.Router();
+const { requireUser } = require("./utils");
+const { createPost } = require("../db");
+
+postsRouter.post("/", async (req, res, next) => {
+  const posts = await createPost();
+
+  res.send({
+    posts,
+  });
+});
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    // const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to the posts route");
