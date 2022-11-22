4d05e8f20f7cae5a48050935fd36fb192ab61c76
MShiers7 <martyshiers7@gmail.com>
2022-11-22 09:42:23 -0600

    Made Patch request

--- index.js
+++ index.js
@@ -36,3 +36,19 @@ server.get("/api", (req, res, next) => {
   console.log("A get request was made to /api");
   res.send({ message: "success" });
 });
+
+server.get("/background/:color", (req, res, next) => {
+  res.send(`
+      <body style="background: ${req.params.color};">
+        <h1>Hello World</h1>
+      </body>
+    `);
+});
+
+server.get("/add/:first/to/:second", (req, res, next) => {
+  res.send(
+    `<h1>${req.params.first} + ${req.params.second} = ${
+      Number(req.params.first) + Number(req.params.second)
+    }</h1>`
+  );
+});
